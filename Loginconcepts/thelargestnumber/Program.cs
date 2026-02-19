472563ebf8e8385865ee076d768c84913a2db9dc
JUAN MANUEL PANIAGUA ALVAREZ <juanpaniagua1157266@correo.itm.edu.co>
2026-02-19 17:44:49 -0500

    thegreatesnumber

--- Loginconcepts/thelargestnumber/Program.cs
+++ Loginconcepts/thelargestnumber/Program.cs
@@ -1,26 +1,26 @@
-﻿Console.Write("Ingrese Tres números diferentes: =>");
+﻿Console.WriteLine("Ingreso tres números diferentes =>");
 Console.Write("Ingrese primer número: ");
-var number1string = Console.ReadLine();
-var number1Int = int.Parse(number1string!);
+var number1String = Console.ReadLine();
+var number1Int = int.Parse(number1String!);
 Console.Write("Ingrese segundo número: ");
-var number2string = Console.ReadLine();
-var number2Int = int.Parse(number2string!);
+var number2String = Console.ReadLine();
+var number2Int = int.Parse(number2String!);
 Console.Write("Ingrese tercer número: ");
-var number3string = Console.ReadLine();
-var number3Int = int.Parse(number3string!);
-if (number1Int != number2Int && number1Int != number3Int)
+var number3String = Console.ReadLine();
+var number3Int = int.Parse(number3String!);
+if (number1Int > number2Int && number1Int > number3Int)
 {
 	Console.WriteLine($"El número mayor es: {number1Int}");
 }
-else if (number2Int != number1Int && number2Int != number3Int)
+else if (number2Int > number1Int && number2Int > number3Int)
 {
 	Console.WriteLine($"El número mayor es: {number2Int}");
 }
-else if (number3Int != number2Int && number3Int != number1Int)
+else if (number3Int > number2Int && number3Int > number1Int)
 {
 	Console.WriteLine($"El número mayor es: {number3Int}");
 }
 else
 {
-	Console.WriteLine("Al menos 2 números son iguales.");
+	Console.WriteLine($"Al menos menos 2 números son iguales.");
 }
